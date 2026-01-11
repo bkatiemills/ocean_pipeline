895fd6db0d62d92606c6ddad16a51eb722f29dbf
Katie Mills <you@example.com>
2026-01-11 15:35:03 -0700

    bugfix

--- tests/integration.py
+++ tests/integration.py
@@ -10,7 +10,8 @@ def test_argovis_pipeline():
 
         mock_data = [profile, copy.deepcopy(profile), copy.deepcopy(profile)] # dupe profile for downsampling
         mock_data[0]['geolocation'] = {"type":"Point","coordinates":[-27.4493,2.32012]} # original, unchanged for reference
-        mock_data[1]['geolocation'] = {"type":"Point","coordinates":[-27.4493,2.33012]} # <0.1 degree too close, should get downsampled out
+        mock_data[1]['geolocation'] = {"type":"Point","coordinates":[-27.4493,2.33012]}
+        mock_data[1]['data'][1] = mock_data[1]['data'][1][0:len(mock_data[1]['data'][1])-1] # <0.1 degree too close and shallower, should get downsampled out
         mock_data[2]['geolocation'] = {"type":"Point","coordinates":[-27.4493,2.44012]} # >0.1 degree far enough, should survive downsampling
         with open(input_path, "w") as f:
             json.dump(mock_data, f)
@@ -27,7 +28,7 @@ def test_argovis_pipeline():
         munged_input_file = glob.glob(selectionfile)
         df = pd.read_parquet(munged_input_file[0])
         assert df["juld"].iloc[0] == 739750.1796412037
-        assert df["latitude"].iloc[0] == 2.32012 
+        assert df["latitude"].iloc[0] == 2.32012
         assert df["longitude"].iloc[0] == 360-27.4493
         assert df["float"].iloc[0] == 1902305
         assert df["cycle"].iloc[0] == '128'
